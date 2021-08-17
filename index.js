030474e3639ba05ec116ae923d980d904aab0864
unknown <doliveirasantos98@gmail.com>
2021-08-16 23:06:41 -0300

    feat: Criando um Básico Tutorial

--- index.js
+++ index.js
@@ -35,8 +35,10 @@ typeof(conc);           // number, string, boolean, function, object, null e und
 let myName = 'Diego'
 let myNumber = '23'
 
-console.log(myName)
-console.log(typeof(myNumber))       // Ira retornar uma string, mesmo sendo um numero
+//console.log(myName)
+//console.log(typeof(myNumber))             // Ira retornar uma string, mesmo sendo um numero
+//console.log(myName.length)                // Informa a quantidade de numero de letras
+//console.log(typeof(myName.length))        // É um number o typeof
 
 /*-------------------------------------------------------------------------------------------------------*/
 
@@ -93,5 +95,55 @@ let obj = [
 
 // Encontrando especificamente um objeto
 
-let firstName = obj[0].firstName    // Pega o valor da key especifica
+let firstName = obj[0].firstName    // Pega o valor(value) da propriedade(key) especificada do objeto
 //console.log(firstName)
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+// Função
+function firstFunction(){
+    var first = '1º'
+    return first
+}
+var first                       // variavel Undefined
+
+//console.log(firstFunction());
+//console.log(toma)
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+// IF e ELSE
+// Tabela com alguns elementos unicos do IF
+
+/*
+    == igualdade
+    === estritamente igualdade (considerando até o typeof)
+    != diferente
+    !== estritamente diferente
+    < menor
+    > maior
+    <= menor ou igual
+    >= maior ou igual
+    && sinal de 'E'
+    || sinal de 'OU'
+*/
+
+/*
+let a = 1;
+let b = 3;
+let c = 2;
+
+if(a == b){                 // if é um 'se' que dentro encontra-se uma condição
+    var sum2 = a + b
+    console.log(sum2)
+} else if(a == c){          // else if é um 'senão se', ele também tem condição
+    var subt2 = a - c
+    console.log(subt2)
+} else{                     // else é o 'senão', else não tem condição
+    console.log('Não quero pensar')
+}
+*/
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+// Loops
